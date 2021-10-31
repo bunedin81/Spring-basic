cc93c405dd432e5b13f4c511c49793dd736eb234
Bunedin <bunedin@Bunedinui-MacBookAir.local>
2021-10-31 12:20:28 +0900

    pure-java-basic-code

---
+++ src/test/java/hello/core/member/MemberServiceTest.java
@@ -0,0 +1,20 @@
+package hello.core.member;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MemberServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+
+    @Test
+    void join() {
+        //given
+        Member memberA = new Member(1L, "MemberA", Grade.VIP);
+        //when
+        memberService.join(memberA);
+        Member member = memberService.findMember(1L);
+        //then
+        Assertions.assertThat(memberA).isEqualTo(member);
+    }
+}
